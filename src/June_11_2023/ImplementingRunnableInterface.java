d11e791f73f54976bbaa191d4e7e93daaa772322
hemanth1699 <vadakattuhemanth1699@gmail.com>
2023-06-14 15:33:42 -0500

    files added.

---
+++ src/June_11_2023/ImplementingRunnableInterface.java
@@ -0,0 +1,23 @@
+package June_11_2023;
+
+public class ImplementingRunnableInterface implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Implementing Runnable Interface  :: " + i);
+
+        }
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Create a Thread by Implementing Runnable Interface");
+        ImplementingRunnableInterface implementingRunnableInterface = new ImplementingRunnableInterface();
+        Thread thread = new Thread(implementingRunnableInterface);
+        thread.start();
+
+    }
+}
